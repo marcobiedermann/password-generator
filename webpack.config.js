89ec8ad1f0bab8d9604123c221acab207cca3a8f
Marco Biedermann <marco.biedermann@gmx.de>
2019-04-04 09:11:01 +0200

    chore: webpack
    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, 'client/assets/js/index'),
+  },
+  output: {
+    filename: 'assets/js/[name].js',
+    path: path.resolve(__dirname, 'server/public'),
+  },
+  module: {
+    rules: [],
+  },
+  plugins: [],
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+    ],
+  },
+};
