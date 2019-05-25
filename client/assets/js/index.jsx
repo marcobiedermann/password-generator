7c50c151c6c25d561fd38b2f76afdfcb2b45b130
Marco Biedermann <marco.biedermann@gmx.de>
2019-05-25 21:34:34 +0200

    feat: markup
    add markup structure

--- client/assets/js/index.jsx
+++ client/assets/js/index.jsx
@@ -1,10 +1,47 @@
+import {
+  Card, CardContent, Checkbox, FormControlLabel, TextField,
+} from '@material-ui/core';
 import React from 'react';
 import { render } from 'react-dom';
 
 const App = () => (
-  <div>
-    Hello, world!
-  </div>
+  <Card>
+    <CardContent>
+      <form>
+        <div>
+          <TextField />
+        </div>
+        <div>
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Numbers"
+            value="numbers"
+          />
+        </div>
+        <div>
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Symbols"
+            value="symbols"
+          />
+        </div>
+        <div>
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Uppercase"
+            value="uppercase"
+          />
+        </div>
+        <div>
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Exclude Similar Characters"
+            value="excludeSimilarCharacters"
+          />
+        </div>
+      </form>
+    </CardContent>
+  </Card>
 );
 
 render(<App />, document.getElementById('root'));
